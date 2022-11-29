422ffd92a5175b7b345f81512bbf189ba0e10796
Irina Tsarevski <irinatsarevski@icloud.com>
2022-11-29 11:43:14 +0100

    added Example 1

--- HomeWork01/Program.cs
+++ HomeWork01/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Домашняя работа по первому семинару С#
+
+/*
+Задача 2.
+Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+Console.WriteLine("Input first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2)
+{
+    Console.WriteLine($"{num1}  is great than {num2}, max = {num1}");
+}
+if(num1 < num2)
+{
+    Console.WriteLine($"{num2} is great than {num1}, max = {num2}");
+}
+if(num1 == num2)
+{
+    Console.WriteLine($"The numbers are equal, {num1} = {num2}");
+}
+/*
+Задача 4.
+Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+/*
+Задача 6.
+Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+/*
+Задача 8.
+Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
